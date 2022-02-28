ce0681acd56a1b5b1df828b4147460e4a23e28f3
Akadic <akadic01@gmail.com>
2022-02-28 21:54:49 +0100

    Drag state

--- src/PullToRefresh/PullToRefresh.tsx
+++ src/PullToRefresh/PullToRefresh.tsx
@@ -1,5 +1,4 @@
 import { useDrag, useScroll } from '@use-gesture/react'
-import cn from 'classnames'
 import { Children, useCallback, useState } from 'react'
 import { animated, useSpring } from 'react-spring'
 
@@ -8,6 +7,11 @@ import styles from './PullToRefresh.module.scss'
 
 import RefreshIcon from "assets/frame1.svg"
 
+
+type DragState = {  // TODO Statemachine
+    enabled: boolean
+    started: boolean
+}
 
 function getScrollParent(node) {
     const isElement = node instanceof HTMLElement;
@@ -29,47 +33,65 @@ function PullToRefresh({
     onRefresh,
 }: PullToRefreshProps) {
     const items = Children.toArray(children)
-    const [draggable, setDraggable] = useState<boolean>(false)
+    const [state, setState] = useState<DragState>({ 
+        enabled: false, 
+        dragging: false
+    })
     const [scroller, setScroller] = useState<HTMLElement>()
     const ref = useCallback(node => {
         if (node !== null) {
+            setState(state => ({ ...state, enabled: node.scrollTop === 0 }))
             setScroller(getScrollParent(node))
         }
     }, []);
 
-    const [{ height }, api] = useSpring(
-        () => ({ height: 0 }))
+    const [{ top }, api] = useSpring(
+        () => ({ top: -159 }))
 
     const bind = useDrag(
-        ({ down, movement: [_, my] }) => {
+        ({ down, movement: [, my], direction: [, dy] }) => {
+            if (state.dragging && !down) {
+                setState(state => ({ ...state, dragging: false }))
+            }
+
+            if (down && dy === 1) {
+                setState(state => ({ ...state, dragging: true }))
+            }
+
+            if (!state.dragging && down && dy === -1) return
+
+            // Drag State
             api.start(() => {
                 return {
-                    height:
-                        down && my < limit
-                            ? my
-                            : down && my >= limit
+                    top:
+                        down && -159 + my < limit
+                            ? -159 + my
+                            : down && -159 + my >= limit
                                 ? limit
-                                : 0,
+                                : -159,
                     immediate: down
                 }
             })
 
-            if (height.get() === limit && !down) {
+            if (top.get() === limit && !down) {
                 onRefresh()
             }
-        }, { enabled: draggable, pointer: { touch: draggable } })
+        }, { axis: "y", enabled: state.enabled, pointer: { touch: state.enabled } })
 
     useScroll(
         ({ xy: [_, y], velocity: [, vy] }) => {
-            setDraggable(y === 0)
-        }, { target: scroller })
+            setState(state => ({ ...state, enabled: y === 0 }))
+        }, { axis: "y", target: scroller })
 
     return (
         <div
+            className={styles.wrapper}
             ref={ref}
             {...bind()}>
-            <animated.div style={{ height }}>
-                {/* <RefreshIcon /> */}
+            <animated.div 
+                className={styles.pullContainer} 
+                style={{ top }}>
+                <RefreshIcon />
             </animated.div>
             {items}
         </div>
