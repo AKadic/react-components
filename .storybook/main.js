a3e2c607f1558701d0cdb59883805be05f5060d1
Akadic <akadic01@gmail.com>
2022-02-28 21:01:01 +0100

    svgr + pull to refresh

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -7,5 +9,28 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-scss"
-  ]
+  ],
+  core: {
+    builder: "webpack5",
+  },
+  webpackFinal: async (config, { configType }) => {
+    const fileLoaderRule = config.module.rules.find(rule => rule.test.test('.svg'));
+    fileLoaderRule.exclude = path.resolve('./assets');
+
+    config.module.rules = [
+      ...(config.module.rules || []),
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack'],
+      }
+    ]
+
+    config.resolve.modules = [
+      ...(config.resolve.modules || []),
+      path.resolve('./'),
+    ];
+
+    return config
+  }
 }
--- src/PullToRefresh/PullToRefresh.tsx
+++ src/PullToRefresh/PullToRefresh.tsx
@@ -1,11 +1,27 @@
-import { useDrag, useGesture, useMove, useScroll } from '@use-gesture/react'
+import { useDrag, useScroll } from '@use-gesture/react'
 import cn from 'classnames'
-import { Children, useCallback, useEffect, useRef, useState } from 'react'
+import { Children, useCallback, useState } from 'react'
 import { animated, useSpring } from 'react-spring'
 
 import type { PullToRefreshProps } from './PullToRefreshProps'
 import styles from './PullToRefresh.module.scss'
 
+import RefreshIcon from "assets/frame1.svg"
+
+
+function getScrollParent(node) {
+    const isElement = node instanceof HTMLElement;
+    const overflowY = isElement && window.getComputedStyle(node).overflowY;
+    const isScrollable = overflowY !== 'visible' && overflowY !== 'hidden';
+
+    if (!node) {
+        return null;
+    } else if (isScrollable && node.scrollHeight >= node.clientHeight) {
+        return node;
+    }
+
+    return getScrollParent(node.parentNode) || window;
+}
 
 function PullToRefresh({
     children,
@@ -13,6 +29,13 @@ function PullToRefresh({
     onRefresh,
 }: PullToRefreshProps) {
     const items = Children.toArray(children)
+    const [draggable, setDraggable] = useState<boolean>(false)
+    const [scroller, setScroller] = useState<HTMLElement>()
+    const ref = useCallback(node => {
+        if (node !== null) {
+            setScroller(getScrollParent(node))
+        }
+    }, []);
 
     const [{ height }, api] = useSpring(
         () => ({ height: 0 }))
@@ -34,11 +57,20 @@ function PullToRefresh({
             if (height.get() === limit && !down) {
                 onRefresh()
             }
-        }, { pointer: { touch: true } })
+        }, { enabled: draggable, pointer: { touch: draggable } })
+
+    useScroll(
+        ({ xy: [_, y], velocity: [, vy] }) => {
+            setDraggable(y === 0)
+        }, { target: scroller })
 
     return (
-        <div {...bind()}>
-            <animated.div style={{ height }} />
+        <div
+            ref={ref}
+            {...bind()}>
+            <animated.div style={{ height }}>
+                {/* <RefreshIcon /> */}
+            </animated.div>
             {items}
         </div>
     )
